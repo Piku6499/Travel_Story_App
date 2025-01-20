222b3103a368c9fe1a4cd0aeb52bd248fbffa38b
Priya Das <23m0315@iitb.ac.in>
2025-01-20 15:41:42 +0530

    first commit

---
+++ frontend/travel-story-app/src/pages/Home/AddEditTravelStory.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddEditTravelStory = ({
+    storyInfo,
+    type,
+    onClose,
+    getAllTravelStories,})=> {
+    return (
+        <div>AddEditTravelStory</div>
+    )
+}
+
+export default AddEditTravelStory
